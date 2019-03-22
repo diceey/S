40a6c9034f033c70e5823cca4513925f8c797e66
Bloxy On Github <36968271+BloxyGithub@users.noreply.github.com>
2019-03-22 23:26:18 +0000

    Update base.py

--- base.py
+++ base.py
@@ -4,10 +4,14 @@ tokens =[]
 
 def open_file(filename):
   data = open(filename, "r").read()
+  data += "<EOF>"
   return data 
 tok = ""
 state = 0
-string= ""
+string = ""
+isexpr = 0
+expr = ""
+n = ""
 def lex(filecontents):
   filecontents = list(filecontents)
   for char in filecontents:
@@ -17,11 +21,24 @@ def lex(filecontents):
       tok = ""
       else:
         tok = " "
-elif tok == "\n":
+elif tok == "\n" or tok == "<EOF>":
+  if expr != "" and isexpr == 1:
+    tokens.append("EXPR:" + expr)
+    expr = ""
+    elif expr != "" and isexpr == 0:
+      tokens.append("NUM:" + expr)
   tok = ""
-      elif tok == "p";
+      elif tok == "p" or tok == "P":
       tokens.append("PRINT")
       tok=""
+      elif tok == "0" or tok == "1" or tok == "2" or tok == "3" or tok == "4" or tok == "5" or tok == "6" or tok == "7" or tok == "8" or tok == "9":
+      print("NUMBER")
+      expr += tok
+      tok = ""
+      elif tok == "+":
+        isexpr = 1
+        expr += tok
+        tok = ""
       elif tok =="\"":
         if state == 0:
           state = 1
@@ -33,6 +50,8 @@ elif tok == "\n":
           elif state == 1:
             string += tok
             tok = ""
+            print(expr)
+            print(tokens)
             return tokens
             #print(tokens)
     
